c79a1e5063a826ae8dacd454a4b04eab4e919e4f
Doug <karaballo.douglas@hotmail.com>
2022-05-27 14:29:38 -0400

    Modificaciones a l os estilos del header en desktop

--- template-parts/components/reusable/element-logo.php
+++ template-parts/components/reusable/element-logo.php
@@ -1,5 +1,5 @@
 <div>
     <a href="<?= home_url('/') ?>">
-        <img src="<?= get_template_directory_uri(). '/dist/assets/img/logo.png' ?>" alt="GamingD">
+        <img class="theme-logo" src="<?= get_template_directory_uri(). '/dist/assets/img/logo.png' ?>" alt="GamingD">
     </a>
 </div>
