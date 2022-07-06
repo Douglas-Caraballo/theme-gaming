c2a05bbfa8462ddbad7ea46239bd3ccaeb24bd1a
Doug <karaballo.douglas@hotmail.com>
2022-07-06 13:03:23 -0400

    Creacion de los estilos de la seccion de esports y modificaciones a los estilos basicos de la seccion de noticias del home

--- template-parts/components/home/element-esport.php
+++ template-parts/components/home/element-esport.php
@@ -11,7 +11,7 @@
 ?>
 
 <div class="wrapper-esport-section">
-    <span><h2>Esport</h2></span>
+    <span class="esport-section-title"><h2>Esport</h2></span>
     <div class="esport-section">
         <?php if($queryEsports -> have_posts()) : ?>
             <?php while($queryEsports -> have_posts()):
@@ -24,8 +24,8 @@
                         </a>
                     </figure>
                     <div class="esport-section__item__info">
-                        <span><?php the_title('<h3><a href='.get_the_permalink().'>','</a></h3>'); ?></span>
-                        <div><?php the_excerpt();?></div>
+                        <span class="esport-section__item__info__wrapper-title"><?php the_title('<h3><a class="esport-section__item__info__title" href='.get_the_permalink().'>','</a></h3>'); ?></span>
+                        <div class="esport-section__item__info__excerp"><?php the_excerpt();?></div>
                         <a class="esport-section__item__info__more" href="<?= esc_url(get_the_permalink($queryEsports->id)); ?>"> Leer Mas >> </a>
                     </div>
                 </article>
