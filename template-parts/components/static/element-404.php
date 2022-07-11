f88fc04f9d72575eac4a8bcf876700eb782dd086
Doug <karaballo.douglas@hotmail.com>
2022-07-11 09:59:04 -0400

    Creacion y modiificacion a los estilos de la page 404

---
+++ template-parts/components/static/element-404.php
@@ -0,0 +1,11 @@
+<div class="wrapper-404">
+    <figure class="figure-404">
+        <img class="figure-404__image" src="<?= get_template_directory_uri().'/dist/assets/img/404.png'; ?>" alt="">
+    </figure>
+    <div class="content-404">
+        <p class="content-404__menssage">Upssss!! AFK</p>
+        <a class="content-404__redirect" href="<?= home_url('/') ?>">
+            Inicio
+        </a>
+    </div>
+</div>
