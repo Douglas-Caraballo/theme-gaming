655b802da27daccf1495e9789e9ef093841c983d
Doug <karaballo.douglas@hotmail.com>
2022-07-19 11:29:44 -0400

    Modificaciones a los estilos de la search page

--- template-parts/components/search/element-content-search.php
+++ template-parts/components/search/element-content-search.php
@@ -1,19 +1,19 @@
-<article>
-    <?php the_title('<h3><a href='.get_the_permalink().'>','</a></h3>'); ?>
-    <figure>
+<article class="search-item">
+    <?php the_title('<h3 class="search-item__title"><a href='.get_the_permalink().'>','</a></h3>'); ?>
+    <figure class="search-item__figure">
         <?php the_post_thumbnail('', ['class'=>'search-thumbnail']); ?>
     </figure>
-    <div>
-        <div>
+    <div class="search-item__content">
+        <div class="search-item__content__date">
             <span><h5><?php the_date('D m, Y'); ?></h5></span>
             <span><h5><?php the_author(); ?></h5></span>
         </div>
-        <div>
+        <div class="search-item__content__excerpt">
             <?php the_excerpt(); ?>
         </div>
-        <div>
+        <div class="search-item__content__redirect">
             <a href="<?= esc_url(get_permalink()); ?>">Ver</a>
         </div>
-        <span><h6><?php gaming_doug_entry_footer();?></h6></span>
+        <span class="search-item__content__footer"><h6><?php gaming_doug_entry_footer();?></h6></span>
     </div>
 </article>
