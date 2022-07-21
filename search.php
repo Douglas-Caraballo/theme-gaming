328c5c72fc89883c903fe1089c87caba0b24ed87
Doug <karaballo.douglas@hotmail.com>
2022-07-21 10:42:23 -0400

    Modificaciones a los estilos de la search page para la paginacion

--- search.php
+++ search.php
@@ -23,29 +23,32 @@ get_header();
 				</h1>
 			</header><!-- .page-header -->
 			<div class="search-content">
-				<div class="search-page-content">
-					<?php
-						/* Start the Loop */
-						while ( have_posts() ) :
-							the_post();
+				<div class="wrapper-search-page">
+					<div class="search-page-content">
+						<?php
+							/* Start the Loop */
+							while ( have_posts() ) :
+								the_post();
 
-							/**
-							 * Run the loop for the search to output the results.
-							 * If you want to overload this in a child theme then include a file
-							 * called content-search.php and that will be used instead.
-							 */
-							get_template_part( 'template-parts/content', 'search' );
+								/**
+								 * Run the loop for the search to output the results.
+								 * If you want to overload this in a child theme then include a file
+								 * called content-search.php and that will be used instead.
+								 */
+								get_template_part( 'template-parts/content', 'search' );
 
-						endwhile;
+							endwhile;
 
-						the_posts_navigation();
+							else :
 
-						else :
+								get_template_part( 'template-parts/content', 'none' );
 
-							get_template_part( 'template-parts/content', 'none' );
-
-						endif;
-					?>
+							endif;
+						?>
+					</div>
+					<div>
+							<?php gaming_doug_numeric_pagination(); ?>
+					</div>
 				</div>
 				<div class="archive-sidebar">
 					<?php get_sidebar(); ?>
