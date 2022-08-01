2640c43f1ff73730ec57c1063230fe076855367c
Doug <karaballo.douglas@hotmail.com>
2022-08-01 11:29:16 -0400

    Finalizando la seccion de comentarios de la single page

--- comments.php
+++ comments.php
@@ -32,13 +32,13 @@ if ( post_password_required() ) {
 			if ( '1' === $gaming_doug_comment_count ) {
 				printf(
 					/* translators: 1: title. */
-					esc_html__( 'One thought on &ldquo;%1$s&rdquo;', 'gaming-doug' ),
+					esc_html__( 'One Comment', 'gaming-doug' ),
 					'<span>' . wp_kses_post( get_the_title() ) . '</span>'
 				);
 			} else {
-				printf( 
+				printf(
 					/* translators: 1: comment count number, 2: title. */
-					esc_html( _nx( '%1$s thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', $gaming_doug_comment_count, 'comments title', 'gaming-doug' ) ),
+					esc_html( _nx( 'Comment (%1$s)', 'Comments (%1$s)', $gaming_doug_comment_count, 'comments title', 'gaming-doug' ) ),
 					number_format_i18n( $gaming_doug_comment_count ), // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 					'<span>' . wp_kses_post( get_the_title() ) . '</span>'
 				);
