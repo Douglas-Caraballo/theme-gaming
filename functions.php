e701f76d30f4ecf2469a2f6de584eb3bc3d4edb8
Doug <karaballo.douglas@hotmail.com>
2022-08-12 08:55:01 -0400

    Cambios a los archivos para descartar la api de riot

--- functions.php
+++ functions.php
@@ -213,17 +213,3 @@ require get_template_directory(). '/inc/pagination.php';
  * Register navigation post in the single page
  */
 require get_template_directory(). '/inc/navigation-post.php';
-//------------------------Funcion api lol ---------------------------//
-
-wp_enqueue_script('api_lol_conect', get_template_directory_uri().'/src/js/esports.js', array(), '1.0', true);
-/*
-add_filter("script_loader_tag", "add_module_to_my_script", 10, 3);
-
-function add_module_to_my_script($tag, $handle, $src){
-	if ("api_lol_conect" === $handle) {
-        $tag = '<script type="module" src="' . esc_url($src) . '"></script>';
-    }
-
-    return $tag;
-}
-*/
